b0387e97576ca869a9fac7b514bbaec048058139
Chris O <coshea@users.noreply.github.com>
2019-11-03 17:17:41 -0500

    EPI 5.2

--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -1,9 +1,18 @@
 from test_framework import generic_test
 
-
+# 5.2
 def plus_one(A):
-    # TODO - you fill in here.
-    return []
+    A[-1] += 1 #increment last digit by 1
+    
+    for i in reversed(range(1, len(A))):
+        if A[i] != 10:
+            break # leave if no carry
+        A[i] = 0
+        A[i-1] += 1
+    if A[0] == 10:
+        A[0] = 1
+        A.append(0) # add zero to last digit
+    return A
 
 
 if __name__ == '__main__':
