689a5aed0a4964e42deea7360716e45800c24215
Chris O <coshea@users.noreply.github.com>
2019-11-10 20:43:03 -0500

    EPI Two sum, Leetcode #1

--- epi_judge_python/is_string_palindromic_punctuation.py
+++ epi_judge_python/is_string_palindromic_punctuation.py
@@ -1,23 +1,24 @@
 from test_framework import generic_test
 
+# Leetcode #125
+
 
 def is_palindrome(s):
     start, end = 0, len(s)-1
-    
+
     while start < end:
         if not s[start].isalnum():
-            start +=1
+            start += 1
             continue
         if not s[end].isalnum():
-            end -=1
+            end -= 1
             continue
         if s[start].lower() != s[end].lower():
             return False
-        
+
         start += 1
         end -= 1
-        
-        
+
     return True
 
 
--- epi_judge_python/two_sum.py
+++ epi_judge_python/two_sum.py
@@ -1,9 +1,22 @@
 from test_framework import generic_test
 
 
-def has_two_sum(A, t):
-    # TODO - you fill in here.
-    return True
+def has_two_sum(nums, target: int):
+    sums = {}
+    result = []
+
+    for i in range(len(nums)):
+        # because you can use same number twice, check not in first
+        # differs from leetcode problem
+        if not nums[i] in sums:
+            sums[target-nums[i]] = i
+
+        if nums[i] in sums:
+            result.append(sums[nums[i]])
+            result.append(nums[i])
+            return True
+
+    return False
 
 
 if __name__ == '__main__':
