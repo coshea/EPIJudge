f1a8052da1bb967ba78b9900eba1bbf22f8182e3
Chris O <coshea@users.noreply.github.com>
2019-11-11 20:54:25 -0500

    EPI 12.1. Leetcode #266 - Palindrome Permutation

--- epi_judge_python/is_string_permutable_to_palindrome.py
+++ epi_judge_python/is_string_permutable_to_palindrome.py
@@ -1,13 +1,31 @@
 from test_framework import generic_test
+from collections import Counter
+
+# 12.1
+# Leetcode #266 - Palindrome Permutation
 
 
-def can_form_palindrome(s):
-    # TODO - you fill in here.
-    return True
+def can_form_palindrome_book(s):
+    return sum(v % 2 for v in Counter(s).values()) <= 1
+
+
+def can_form_palindrome_mine(s):
+    _dict: set = set()
+    for c in s:
+        if c in _dict:
+            _dict.remove(c)
+        else:
+            _dict.add(c)
+
+    if len(s) % 2 == 1 and len(_dict) == 1:
+        return True
+    if len(s) % 2 == 0 and len(_dict) == 0:
+        return True
+    return False
 
 
 if __name__ == '__main__':
     exit(
         generic_test.generic_test_main(
             "is_string_permutable_to_palindrome.py",
-            'is_string_permutable_to_palindrome.tsv', can_form_palindrome))
+            'is_string_permutable_to_palindrome.tsv', can_form_palindrome_mine))
