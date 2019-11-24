7b36aa026d943b25705ba996665677a30b8a9c69
Chris O'Shea <christopher.d.oshea@gmail.com>
2019-11-24 16:42:12 -0500

    EPI 14.1 Test if BST. Page 214

--- epi_judge_python/is_tree_a_bst.py
+++ epi_judge_python/is_tree_a_bst.py
@@ -1,5 +1,6 @@
 from test_framework import generic_test
 from binary_tree_node import BinaryTreeNode
+from collections import deque, namedtuple
 
 
 def is_binary_tree_bst(tree: BinaryTreeNode, low_range=float('-inf'), high_range=float('inf')):
@@ -16,7 +17,32 @@ def is_binary_tree_bst(tree: BinaryTreeNode, low_range=float('-inf'), high_range
             and is_binary_tree_bst(tree.right, tree.data, high_range))
 
 
+def is_binary_tree_bst_queue(tree: BinaryTreeNode, low_range=float('-inf'), high_range=float('inf')):
+    """
+    EPI 14.1 Test if BST. Page 214
+    """
+    if not tree:
+        return True
+
+    QueueEntry = namedtuple('QueueEntry', ('node', 'lower', 'higher'))
+
+    bfs_queue = deque([QueueEntry(tree, float('-inf'), float('inf'))])
+
+    while bfs_queue:
+        item = bfs_queue.popleft()
+
+        if not item.lower <= item.node.data <= item.higher:
+            return False
+
+        bfs_queue.extend(
+            QueueEntry(item.node.left, item.lower, item.node.data))
+        bfs_queue.append(
+            QueueEntry(item.node.right, item.node.data, item.higher))
+
+    return True
+
+
 if __name__ == '__main__':
     exit(
         generic_test.generic_test_main("is_tree_a_bst.py", 'is_tree_a_bst.tsv',
-                                       is_binary_tree_bst))
+                                       is_binary_tree_bst_queue))
