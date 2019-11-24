4d42fd83ad4a2e37c733226bbb45b481412b8b77
Chris O'Shea <christopher.d.oshea@gmail.com>
2019-11-24 14:12:37 -0500

    EPI 10.5 Online Median. Page 148

--- epi_judge_python/online_median.py
+++ epi_judge_python/online_median.py
@@ -1,9 +1,29 @@
 from test_framework import generic_test
+import heapq
+from typing import List, Iterator
 
 
-def online_median(sequence):
-    # TODO - you fill in here.
-    return []
+def online_median(sequence: Iterator[int]) -> List[float]:
+    """
+    EPI 10.5 Online Median. Page 148
+    """
+
+    # min heap to keep left values, max heap to keep right values
+    min_heap: List[int] = []
+    max_heap: List[int] = []
+    result = []
+    for s in sequence:
+        heapq.heappush(max_heap, -heapq.heappushpop(min_heap, s))
+
+        if len(max_heap) > len(min_heap):
+            heapq.heappush(min_heap, -heapq.heappop(max_heap))
+
+        if len(max_heap) == len(min_heap):
+            result.append(((max_heap[0] * -1) + min_heap[0]) / 2)
+        else:
+            result.append(min_heap[0])
+
+    return result
 
 
 def online_median_wrapper(sequence):
