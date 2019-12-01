b58bf220903cb638ecf216048056a4cdf93c6c1f
Chris O'Shea <christopher.d.oshea@gmail.com>
2019-12-01 17:42:57 -0500

    EPI 12.5 Nearest repeated entries in an array

--- epi_judge_python/nearest_repeated_entries.py
+++ epi_judge_python/nearest_repeated_entries.py
@@ -1,9 +1,23 @@
 from test_framework import generic_test
+from typing import List
 
 
-def find_nearest_repetition(paragraph):
-    # TODO - you fill in here.
-    return 0
+def find_nearest_repetition(paragraph: List[str]) -> int:
+    """
+    EPI 12.5 Nearest repeated entries in an array
+    """
+    words: Dict[str, int] = {}
+    min_dist = float('inf')
+
+    for i, word in enumerate(paragraph):
+        if word not in words:
+            words[word] = i
+        else:
+            dist = i - words[word]
+            min_dist = min(dist, min_dist)
+            words[word] = i
+
+    return min_dist if min_dist != float('inf') else -1
 
 
 if __name__ == '__main__':
